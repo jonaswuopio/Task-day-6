9d1da02b33eed5e912bea9ffa4d4a2179d9ed145
jonaswuopio <jonas.wuopio@ki.se>
2024-11-26 14:28:26 +0100

    This is as close I can get. Do we have the same data?

---
+++ Plot 2.R
@@ -0,0 +1,24 @@
+###Plot 2
+
+###Load data
+
+df <- read.csv("./data_plot2.csv")
+
+### Scatterplot Area vs population with smooth line
+
+ggplot(df, aes(x = area, y = poptotal, color = state, size = popdensity)) +  
+  geom_point() +
+  geom_smooth(method = "loess", se = FALSE, color = "blue", size = 1) +
+  scale_size_continuous(range = c(1, 5)) +
+  labs(title = "Scatterplot Area vs population",
+       x = "Area",
+       y = "Population") +
+  theme(panel.border = element_rect(color = "black", fill = NA, linewidth = 1),
+    panel.background = element_rect(fill = "white"),  # Set background to white
+    panel.grid.major = element_line(color = "gray90", linewidth = 0.5),  # Major grid lines in light grey
+    panel.grid.minor = element_line(color = "gray95", linewidth = 0.25))
+
+
+
+
+  
