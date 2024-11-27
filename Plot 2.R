949b896f7f2f74b1c37bff8042477c50de0962ae
jonaswuopio <jonas.wuopio@ki.se>
2024-11-27 09:40:10 +0100

    With subtitle

--- Plot 2.R
+++ Plot 2.R
@@ -9,11 +9,14 @@ df <- read.csv("./data_plot2.csv")
 ggplot(df, aes(x = area, y = poptotal, color = state, size = popdensity)) +  
   geom_point() +
   geom_smooth(method = "loess", se = FALSE, color = "blue", size = 1) +
-  scale_size_continuous(range = c(1, 5)) +
-  labs(title = "Scatterplot Area vs population",
-       x = "Area",
-       y = "Population") +
-  theme(panel.border = element_rect(color = "black", fill = NA, linewidth = 1),
+  labs(
+    title = "Scatterplot", 
+    subtitle = "Area vs population",
+    x = "Area",
+    y = "Population") +
+  theme(
+    plot.subtitle = element_text(size = 10),
+    panel.border = element_rect(color = "black", fill = NA, linewidth = 1),
     panel.background = element_rect(fill = "white"),  # Set background to white
     panel.grid.major = element_line(color = "gray90", linewidth = 0.5),  # Major grid lines in light grey
     panel.grid.minor = element_line(color = "gray95", linewidth = 0.25))
